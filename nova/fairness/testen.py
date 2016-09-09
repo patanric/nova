322d62c3deb1e880c60710cf9d160029aa745309
rpatan <riccardo.patane@uzh.ch>
2016-09-09 20:02:14 +0200

    add test file

--- nova/fairness/testen.py
+++ nova/fairness/testen.py
@@ -1,4 +1,4 @@
 from sqlalchemy import MetaData, Table
 meta = MetaData()
 meta.bind = 'mysql+pymysql://nova:wasserfall@openstack-controller/nova_api'
-compute_nodes = Table('compute_nodes', meta, autoload=True)
+compute_nodes = Table('tes', meta, autoload=True)
